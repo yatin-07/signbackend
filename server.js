a7606cf103966c70979c859d0aaabe640f06f095
yatin-07 <nish07ravenclaw@gmail.com>
2021-08-24 21:06:09 +0530

    register route edit

--- server.js
+++ server.js
@@ -1,26 +1,25 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const authrouter = require('./routes/auth');
+const authrouter = require("./routes/auth");
 const router = require("./routes/route");
-const cors = require('cors');
+const cors = require("cors");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 require("dotenv").config();
 
-app.use(cors())
+app.use(cors());
 app.use("/user", authrouter);
-app.use("/users", authrouter);
-app.use('/api', router)
-
+app.use("/user", authrouter);
+app.use("/api", router);
 
 if (process.env.DataBase_Connection) {
-    try {
-      mongoose.connect(process.env.DataBase_Connection, {
-        useNewUrlParser: true,
-      });
-    } catch (err) {
-      console.log("errr  while connecting db", "errr");
-    }
+  try {
+    mongoose.connect(process.env.DataBase_Connection, {
+      useNewUrlParser: true,
+    });
+  } catch (err) {
+    console.log("errr  while connecting db", "errr");
   }
+}
 app.listen(3001, () => console.log("server is running"));
